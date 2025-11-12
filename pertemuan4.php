c726be6cd3db788449c7ddff5c8d88e893d2a39b
nayssilaif24 <if24.nayssilapramudiana@mhs.ubpkarawang.ac.id>
2025-11-13 00:25:15 +0700

    Create pertemuan4.php

---
+++ pertemuan4.php
@@ -0,0 +1,22 @@
+<?php
+
+class LuasLingkaran{
+
+    public const phi = 3.14;
+    public int $jari;
+
+    public function tampil($nama ='ban')  {
+        $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
+        echo "Lingkaran {$nama} hasilnya adalah: ". $rumus;
+    }
+
+    public static function testing() {
+        echo "<br/>";
+        echo "ini dari static";
+    }
+}
+
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 7;
+$lingkaran->tampil('roda'); //panggil method
+LuasLingkaran::testing();
